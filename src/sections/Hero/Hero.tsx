f8419c736ec2aed3ec45ed80c3b54be5b314899c
SushilBajracharya01 <bajracharya.sushil01@gmail.com>
2023-10-01 20:16:19 +0545

    feat: added Hero image in hero section"

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,4 +1,5 @@
 import FollowForm from "../../components/HeroFollowForm";
+import HeroImg from '../../assets/images/hero.png';
 
 /**
  * 
@@ -6,7 +7,7 @@ import FollowForm from "../../components/HeroFollowForm";
 export default function HeroSection() {
     return (
         <section className="">
-            <div className="grid relative lg:max-w-7xl py-36 mx-auto lg:gap-8 xl:gap-0 px-6 2xl:px-0 lg:py-52 lg:gird-col-12">
+            <div className="grid relative lg:max-w-7xl py-36 mx-auto lg:gap-8 xl:gap-0 px-6 2xl:px-0 lg:py-24 lg:gird-col-12">
                 <div
                     className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
                     aria-hidden="true"
@@ -20,17 +21,21 @@ export default function HeroSection() {
                     />
                 </div>
 
-                <div className="mr-auto place-self-center lg:col-span-7">
-                    <h1 className="max-w-2xl mb-10 text-4xl text-gray-800 font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl font-generic">
-                        Connect, Communicate, and Create with <span className="text-primary-600">Circle</span>
-                    </h1>
+                <div className="flex justify-between">
+                    <div className="mr-auto place-self-center lg:col-span-7">
+                        <h1 className="max-w-2xl mb-10 text-4xl text-gray-800 font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl font-generic">
+                            Connect, Communicate, and Create with <span className="text-primary-600">Circle</span>
+                        </h1>
 
-                    <p className="max-w-2xl mb-6 font-light text-gray-800 lg:mb-12 md:text-lg lg:text-xl font-asap">
-                        A Hub for Socializing, Discussing Ideas, and Bringing Your Imagination to Life.
-                    </p>
+                        <p className="max-w-2xl mb-6 font-light text-gray-800 lg:mb-12 md:text-lg lg:text-xl font-asap">
+                            A Hub for Socializing, Discussing Ideas, and Bringing Your Imagination to Life.
+                        </p>
 
-                    <p className="text-sm text-gray-600">Stay in the Loop: Follow Circle's Development.</p>
-                    <FollowForm />
+                        <p className="text-sm text-gray-600">Stay in the Loop: Follow Circle's Development.</p>
+                        <FollowForm />
+                    </div>
+
+                    <img className="hidden lg:block" src={HeroImg} />
                 </div>
             </div>
         </section>
